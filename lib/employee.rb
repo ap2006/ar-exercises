c2f5aa2fe39bbb2b4aad99313b47e8e481e30a34
Amita <amita.parikh@gmail.com>
2019-04-16 21:58:18 +0000

    Finished exercises 6 and 7

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
 
+  belongs_to :store
+
+  validates :first_name, :last_name, presence: true
+  validates :hourly_rate, numericality: {only_integer: true}, inclusion: {in: 40..200}
+  validates_presence_of :store
+
 end
