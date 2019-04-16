7e12abfe8c572d40ed397ccdc4ef6b3efc332f33
Amita <amita.parikh@gmail.com>
2019-04-16 17:05:19 +0000

    Completed exercise 2, changed a store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+# binding.pry
+# puts @store1.inspect
+
+@store1.update(name: "Bergdorf Goodman")
+# puts @store1.inspect
